bc72d684d03727641b25670922b4fa8d8c06a274
kurtamodia-opswerks <km.amodia@ssed-academy.opswerks.com>
2025-08-14 16:19:38 +0800

    chore: update installed apps in settings and project url settings

--- hunter_api/settings.py
+++ hunter_api/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',
+    'django_extensions',
+    'silk',
+    'drf_spectacular',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
