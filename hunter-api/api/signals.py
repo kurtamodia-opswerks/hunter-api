f0dfa262da247f92f04c06f3dfa1efc097ef9ef5
kurtamodia-opswerks <km.amodia@ssed-academy.opswerks.com>
2025-08-31 08:31:48 +0800

    fix: fix cache invalidation to include related models

--- hunter-api/api/signals.py
+++ hunter-api/api/signals.py
@@ -12,31 +12,42 @@ def clear_cache(pattern: str):
 @receiver([post_save, post_delete], sender=Hunter)
 def invalidate_hunter_cache(sender, instance, **kwargs):
     clear_cache("*hunter_list*")
+    clear_cache("*participation_list*")
+    clear_cache("*guild_list*")
+    clear_cache("*raid_list*")
+    clear_cache("*skill_list*")
 
 
 @receiver([post_save, post_delete], sender=Guild)
 def invalidate_guild_cache(sender, instance, **kwargs):
     clear_cache("*guild_list*")
+    clear_cache("*hunter_list*")
 
 
 @receiver([post_save, post_delete], sender=Dungeon)
 def invalidate_dungeon_cache(sender, instance, **kwargs):
     clear_cache("*dungeon_list*")
+    clear_cache("*raid_list*")
 
 
 @receiver([post_save, post_delete], sender=Raid)
 def invalidate_raid_cache(sender, instance, **kwargs):
     clear_cache("*raid_list*")
+    clear_cache("*participation_list*")
+    clear_cache("*hunter_list*")
 
 
 @receiver([post_save, post_delete], sender=RaidParticipation)
 def invalidate_raid_participation_cache(sender, instance, **kwargs):
     clear_cache("*participation_list*")
+    clear_cache("*raid_list*")
+    clear_cache("*hunter_list*")
 
 
 @receiver([post_save, post_delete], sender=Skill)
 def invalidate_skill_cache(sender, instance, **kwargs):
     clear_cache("*skill_list*")
+    clear_cache("*hunter_list*")
 
 
 # Add the leader as a member when a guild is created
